f3c1ed6a32d3a0973c9359f5e592d0d01e15c2c4
Emmanuel Ventura <emmanuel@venturavillanueva.com>
2013-08-25 21:38:06 +0200

    Mise à jour CSS
    Ajout du traitement du formulaire en mode Smartphone
    Ajout de traitements javascript

--- js/projet/jquery.common.js
+++ js/projet/jquery.common.js
@@ -1,3 +1,20 @@
 $(window).ready(function() {
+
+	// Affichage des liens masqués ne fonctionnant qu'avec le js
+	$(".nojs").removeClass('nojs');
+
+	// Retour en haut de page
+	$('#print').on('click',function(e){
+		e.preventDefault();
+		print();
+	});
+
+	// Retour en haut de page
+	$('#up').on('click',function(e){
+		e.preventDefault();
+		$('html,body').animate({scrollTop:0},'slow');
+	});
+
+	// Active la modale sur les images
 	$("a[rel='atm-enlarge']").fancybox();
 });
